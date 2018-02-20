99411e54cbe5e0223db5410d091be793b024e7b2
midraed <guillermo.olmedo@gmail.com>
2018-02-20 10:06:53 +0100

    travis

--- _deploy.sh
+++ _deploy.sh
@@ -8,9 +8,3 @@ set -e
 git config --global user.email "guillermo.olmedo@gmail.com"
 git config --global user.name "midraed"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
-cd book-output
-cp -r ../_book/* ./
-git add --all *
-git commit -m"Update the book" || true
-git push -q origin gh-pages
